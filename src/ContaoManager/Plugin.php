c0d01552326368fda4a9a903dc1d75c258e21176
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-02-11 13:45:31 +0100

    Plugin.php updated

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -7,14 +7,13 @@ namespace MirandaLeyva\ContaoPopupBundle\ContaoManager;
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
-use Contao\ManagerPlugin\Bundle\Config\BundleConfigInterface;
 
 final class Plugin implements BundlePluginInterface
 {
     public function getBundles(): array
     {
         return [
-            BundleConfig::create(\MirandaLeyva\ContaoPopupBundle\MirandaLeyvaContaoPopupBundle::class)
+            (new BundleConfig(\MirandaLeyva\ContaoPopupBundle\MirandaLeyvaContaoPopupBundle::class))
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
