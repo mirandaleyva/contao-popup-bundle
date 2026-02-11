4ded72076e20238da6c11ee465a6792070798f75
mirandaleyva <miranda.leyva.delgado@gmail.com>
2026-02-11 14:09:19 +0100

    Lauft

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -7,10 +7,12 @@ namespace MirandaLeyva\ContaoPopupBundle\ContaoManager;
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use MirandaLeyva\ContaoPopupBundle\ContaoPopupBundle;
 
 final class Plugin implements BundlePluginInterface
 {
-    public function getBundles(): array
+    public function getBundles(ParserInterface $parser): array
     {
         return [
             (new BundleConfig(\MirandaLeyva\ContaoPopupBundle\MirandaLeyvaContaoPopupBundle::class))
--- src/MirandaLeyvaContaoPopupBundle.php
+++ src/MirandaLeyvaContaoPopupBundle.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace MirandaLeyva\ContaoPopupBundle\ContaoManager;
+namespace MirandaLeyva\ContaoPopupBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
